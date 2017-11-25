05f37c448a0601cefb48d52833acf7a45d3af3d4
Timothy KEYNES <tkeynes@e1r12p18.42.fr>
2017-11-25 18:52:14 +0100

    Everything working - :/ 80

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tkeynes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 16:56:22 by tkeynes           #+#    #+#             */
-/*   Updated: 2017/11/22 19:32:04 by tkeynes          ###   ########.fr       */
+/*   Updated: 2017/11/25 18:50:58 by tkeynes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,14 @@ int		get_next_line(int const fd, char **line)
 	t_list	*current_elem;
 	int		ret;
 
-	if (!line)
+	if (!line || BUFF_SIZE == 0)
 		return (-1);
 	if (!find_fd_list(fd))
 		current_elem = ft_list_push_back(&g_fds, 0);
 	else
 		current_elem = find_fd_list(fd);
 	current_elem->content_size = fd;
-	if ((ret = iterations(current_elem, line, fd)) == -100)
-		(void)ret;
-	else
+	if (!((ret = iterations(current_elem, line, fd)) == -100))
 		return (ret);
 	if ((ft_strchr(*line, '\n')))
 	{
@@ -42,26 +40,25 @@ int		iterations(t_list *current_elem, char **line, int fd)
 	int ret;
 
 	*line = "";
+	ret = 10;
 	if (!current_elem->data || ft_strlen((char *)current_elem->data) == 0)
-	{
 		ret = read_buffer(fd, line);
-		if (ret == -1)
-			return (-1);
-		if (!(*line) || ret == 0)
-			return (0);
-		else if (ret == -2)
-			return (1);
-	}
 	else if (!ft_strchr(current_elem->data, '\n') ||
 !ft_strchr(current_elem->data, 0))
 	{
-		read_buffer(fd, line);
+		ret = read_buffer(fd, line);
 		(*line) = ft_strjoin(current_elem->data, *line);
 		if (!ft_strchr(*line, '\n'))
 			free(current_elem->data);
 	}
 	else
 		(*line) = ft_strdup(current_elem->data);
+	if ((ft_strlen(*line) == 0 && ret == 0) || !(*line))
+		return (0);
+	if (ret == -1)
+		return (-1);
+	if (ret == -2)
+		return (1);
 	return (-100);
 }
 
@@ -99,11 +96,11 @@ int		read_buffer(int const fd, char **final)
 		if ((!(*final = (char *)malloc((BUFF_SIZE + ft_strlen(tmp) + 1)))) ||
 ((ret_read = read(fd, buffer, BUFF_SIZE)) == -1))
 			return (-1);
+		buffer[ret_read] = 0;
 		ft_strcpy(*final, tmp);
 		ft_strcat(*final, buffer);
 		free(tmp);
-		if (ret_read == 0 && (*final)[ft_strlen(*final)] == 0 &&
-				ft_strlen(*final) > 0)
+		if (!ret_read && !(*final)[ft_strlen(*final)] && ft_strlen(*final) > 0)
 			return (-2);
 		if (ret_read == 0)
 			return (0);
